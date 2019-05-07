5d81e300e4a8f9a036f33374eb5756f9aaf8af86
Luiz Renato <luizrenato2005@gmail.com>
2019-05-07 16:37:04 -0300

    - implementacao de filtro por descricao e periodo de data de vencimento.

--- algamoney-api/src/main/java/com/algaworks/algamoney/api/controller/LancamentoResource.java
+++ algamoney-api/src/main/java/com/algaworks/algamoney/api/controller/LancamentoResource.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,6 +27,7 @@ import com.algaworks.algamoney.api.exception.PessoaInexistenteOuInativaException
 import com.algaworks.algamoney.api.exceptionhandler.AlgamoneyExceptionHandler.Erro;
 import com.algaworks.algamoney.api.model.Lancamento;
 import com.algaworks.algamoney.api.repository.LancamentoRepository;
+import com.algaworks.algamoney.api.repository.filter.LancamentoFilter;
 import com.algaworks.algamoney.api.service.LancamentoService;
 
 @RestController
@@ -33,7 +35,7 @@ import com.algaworks.algamoney.api.service.LancamentoService;
 public class LancamentoResource {
 	
 	@Autowired
-	private LancamentoRepository repository;
+	private LancamentoRepository lancamentoRepository;
 	
 	@Autowired
 	private LancamentoService service;
@@ -44,18 +46,17 @@ public class LancamentoResource {
 	@Autowired
 	private MessageSource messageSource;
 	
+	@GetMapping
+	public List<Lancamento> pesquisar(LancamentoFilter lancamentoFilter) {
+		return this.lancamentoRepository.filtrar(lancamentoFilter);
+	}
+	
 	@GetMapping("/{codigo}")
 	public ResponseEntity<?> busca(@PathVariable Long codigo) {
-		Lancamento lancamento = this.repository.findOne(codigo);
+		Lancamento lancamento = this.lancamentoRepository.findOne(codigo);
 		return lancamento!=null ? 
 				ResponseEntity.ok(lancamento) :
 				ResponseEntity.notFound().build();
-	}
-	
-	@GetMapping
-	@ResponseStatus(org.springframework.http.HttpStatus.OK)
-	public List<Lancamento> listaTodos() {
-		return this.repository.findAll();
 	}
 	
 	@PostMapping
