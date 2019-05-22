03576c7708c51a4bb9301062f872cfbfb97d0b91
Luiz Renato <luizrenato2005@gmail.com>
2019-05-22 00:09:03 -0300

    correcoes filtro cookie

--- algamoney-api/src/main/java/com/algaworks/algamoney/api/token/RefreshTokenCookiePreProcessorFilter.java
+++ algamoney-api/src/main/java/com/algaworks/algamoney/api/token/RefreshTokenCookiePreProcessorFilter.java
@@ -8,7 +8,6 @@ import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
-import javax.servlet.ServletRequestWrapper;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -17,14 +16,11 @@ import javax.servlet.http.HttpServletRequestWrapper;
 import org.apache.catalina.util.ParameterMap;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpRequest;
-import org.springframework.http.client.support.HttpRequestWrapper;
-import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.stereotype.Component;
 
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
-public class RefreshTokenPreProcessorFilter implements Filter {
+public class RefreshTokenCookiePreProcessorFilter implements Filter {
 	
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
@@ -33,23 +29,21 @@ public class RefreshTokenPreProcessorFilter implements Filter {
 		//pegar refresh token do cookie
 		// adicionar nos parametros da requisicao
 		String refreshToken = "";
-		HttpServletRequest httpRequest = ((ServletServerHttpRequest)request).getServletRequest();
-		if ("/oauth/token".equals(httpRequest.getRequestURI()) && 
+		HttpServletRequest httpRequest = (HttpServletRequest)request;
+		if ("/oauth/token".equalsIgnoreCase(httpRequest.getRequestURI()) && 
+				"refresh_token".equals(httpRequest.getParameter("grant_type")) &&
 				httpRequest.getCookies()!=null) {
 			Cookie[] cookies = httpRequest.getCookies();
 			for(Cookie cookie : cookies) {
 				if (cookie.getName().equals("refreshToken")) {
 					refreshToken = cookie.getValue();
-					adicionaTokenAoRequest(refreshToken, httpRequest);
+					httpRequest = new MyRequestWrapper(httpRequest, refreshToken);
 				}
 			}
 		}
 		chain.doFilter(httpRequest, response);
 	}
 
-	private void adicionaTokenAoRequest(String refreshToken, HttpServletRequest httpRequest) {
-		
-	}
 
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
@@ -61,19 +55,22 @@ public class RefreshTokenPreProcessorFilter implements Filter {
 		
 	}
 	
-	static class MyRequestWrapper extends ServletRequestWrapper {
+	static class MyRequestWrapper extends HttpServletRequestWrapper {
 
 		private String refreshToken;
 		
-		public MyRequestWrapper(ServletRequest request) {
+		public MyRequestWrapper(HttpServletRequest request, String refreshToken) {
 			super(request);
 			this.refreshToken=refreshToken;
 		}
+		
+		
 
 		@Override
 		public Map<String, String[]> getParameterMap() {
 			ParameterMap<String, String[]> params = new ParameterMap<>();
 			params.put("refresh_token", new String[] {this.refreshToken});
+			params.setLocked(true);
 			return super.getParameterMap();
 		}
 
